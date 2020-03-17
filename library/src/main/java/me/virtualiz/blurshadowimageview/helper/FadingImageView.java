d92814ace3ae1cc4459552ba46a48d51240f0b49
Vivek Verma <vivekverma2499@gmail.com>
2020-03-17 13:02:06 +0530

    Updating FadingImageView -minify -v1.12
    minified -helper class

--- library/src/main/java/me/virtualiz/blurshadowimageview/helper/FadingImageView.java
+++ library/src/main/java/me/virtualiz/blurshadowimageview/helper/FadingImageView.java
@@ -30,21 +30,18 @@ public class FadingImageView extends ImageView {
         this.c = c;
         init();
     }
-
     public FadingImageView(Context c, AttributeSet attrs) {
         super(c, attrs);
 
         this.c = c;
         init();
     }
-
     public FadingImageView(Context c) {
         super(c);
 
         this.c = c;
         init();
     }
-
     private void init() {
         // Enable horizontal fading
         this.setHorizontalFadingEdgeEnabled(true);
@@ -66,64 +63,50 @@ public class FadingImageView extends ImageView {
         paint2 = new Paint();
         paint2.setXfermode(null);
     }
-
     public void setFadeRight(boolean fadeRight) {
         mFadeRight = fadeRight;
     }
-
     public void setFadeLeft(boolean fadeLeft) {
         mFadeLeft = fadeLeft;
     }
-
     public void setFadeTop(boolean fadeTop) {
         mFadeTop = fadeTop;
     }
-
     public void setFadeBottom(boolean fadeBottom) {
         mFadeBottom = fadeBottom;
     }
-
     public void setEdgeLength(int length) {
         this.setFadingEdgeLength(getPixels(length));
     }
-
-
     @Override
     protected float getTopFadingEdgeStrength() {
         return mFadeTop ? 1.0f : 0.0f;
     }
-
     @Override
     protected float getBottomFadingEdgeStrength() {
         return mFadeBottom ? 1.0f : 0.0f;
     }
-
     @Override
     protected float getLeftFadingEdgeStrength() {
         return mFadeLeft ? 1.0f : 0.0f;
     }
-
     @Override
     protected float getRightFadingEdgeStrength() {
         return mFadeRight ? 1.0f : 0.0f;
     }
-
     @Override
     public boolean hasOverlappingRendering() {
         return true;
     }
-
     @Override
     public boolean onSetAlpha(int alpha) {
         return false;
     }
-
     private int getPixels(int dipValue) {
         Resources r = c.getResources();
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
                 dipValue, r.getDisplayMetrics());
     }
-
     @Override
     public void draw(Canvas canvas) {
         Bitmap bitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
