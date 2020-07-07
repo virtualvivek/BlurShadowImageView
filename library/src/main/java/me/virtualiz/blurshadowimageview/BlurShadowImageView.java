2d11ed97916606023b49c2cc3775eab493ed63c5
Vivek Verma <vivekverma2499@gmail.com>
2020-07-07 06:41:13 -0700

    Update BlurShadowImageView.java

--- library/src/main/java/me/virtualiz/blurshadowimageview/BlurShadowImageView.java
+++ library/src/main/java/me/virtualiz/blurshadowimageview/BlurShadowImageView.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      https://github.com/vivekverma007/BlurShadowImageView/blob/master/LICENSE
+ * https://github.com/virtualvivek/BlurShadowImageView/blob/master/LICENSE
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
